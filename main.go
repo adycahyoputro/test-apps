71e49a5bd274a48ebdd12204f24c5a7e40d5605b
adycahyoputro <adycahyoputro@gmail.com>
2025-01-01 21:29:40 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,80 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
-type DbBook struct {
-	Id     int
-	Title  string
-	Author string
-}
-
-var cfg = []DbBook{
-	{Id: 1, Title: "buku sejarah", Author: "budi"},
-	{Id: 2, Title: "buku fisika", Author: "rara"},
-	{Id: 3, Title: "buku ppkn", Author: "nana"},
-}
-
 func main() {
-	http.HandleFunc("/Books", GetBookList)
-	http.HandleFunc("/Book1", GetBookById)
-	// http.HandleFunc("/Book2", InsertBook)
-	// http.HandleFunc("/Book3", UpdateBook)
-	// http.HandleFunc("/Book4", DeleteBook)
-	log.Println("listening on port: 8080 always listening always undestanding")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/Books", helloHandler)
+	
+	log.Println("listening on port: 8081 always listening always undestanding")
+	http.ListenAndServe(":8081", nil)
 
 }
 
-func GetBookList(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method == "GET" {
-		// bookList := repo.Books
-		var result, err = json.Marshal(cfg)
-		fmt.Println(cfg)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(result)
-		return
-		// json.NewEncoder(w).Encode(cfg)
-	}
-	http.Error(w, "status bad request", http.StatusBadRequest)
-}
-
-func GetBookById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method == "GET" {
-		id := r.FormValue("id")
-		idInt, errStr := strconv.Atoi(id)
-		if errStr != nil {
-			fmt.Println(errStr)
-		}
-		var result []byte
-		var err error
-		bookList := cfg
-		for _, value := range bookList {
-
-			if value.Id == idInt {
-				result, err = json.Marshal(value)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				w.Write(result)
-				return
-			}
-		}
-		http.Error(w, "book not found", http.StatusNotFound)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "invalid request method")
 		return
 	}
-	http.Error(w, "status bad request", http.StatusBadRequest)
+
+	// fmt.Fprintf(w, "hello world")
+	fmt.Fprint(w, "a")
 }
