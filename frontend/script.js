cd0d016091266a58ac9d47217f238518388561b8
adycahyoputro <adycahyoputro@gmail.com>
2025-01-07 15:10:41 +0700

    update frontend

--- frontend/script.js
+++ frontend/script.js
@@ -10,36 +10,44 @@
 //     contentType: "application/json",
 //     data: myJson,
 //     success: function (result) {
-//       console.log(result);
-//   if (result.data != "") {
-//     alert("success");
-//   } else {
-//     alert("failed");
-//   }
+//       // console.log(result);
+//       console.log(result.data.error);
+//       // console.log(result.error);
+//       // alert("success");
+//       if (result.data.error != "") {
+//         alert("success");
+//       } else {
+//         alert("failed");
+//       }
 //     },
 //   });
 // });
 var x = document.getElementById("save");
 x.addEventListener("click", AddData);
 
-var obj = {};
-obj.username = $("#exampleInputText1").val();
-obj.email = $("#exampleInputEmail1").val();
 async function AddData() {
+  var obj = {};
+  obj.username = $("#exampleInputText1").val();
+  obj.email = $("#exampleInputEmail1").val();
+  var data = JSON.stringify(obj);
+  // try {
   let endpoint = "http://192.168.157.160:8082/register";
   const request = await fetch(endpoint, {
     method: "POST",
-    //   body: data,
-    body: JSON.stringify(obj),
+    // body: JSON.stringify(obj),
+    body: data,
     headers: { "Content-Type": "application/json" },
   });
 
   const response = await request.json();
-  console.log(response);
-  //   if (response.ok) {
-  //     console.log(response);
-  //   } else {
-  //     console.log(`An error with status code ${response.status} occured`);
-  //   }
+  // console.log(response);
+
+  if (response.data) {
+    alert("success register");
+    console.log(response);
+  }
+  if (response.error) {
+    alert("failed");
+    console.log(response);
+  }
 }
-AddData();
